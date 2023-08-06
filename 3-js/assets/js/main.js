ccc773a0b5c778507e2f11963e67cbdae6cff85a
Nicole <niipimenta@hotmail.com>
2023-08-05 22:48:20 -0300

    feat: resolução de dois cases

--- 3-js/assets/js/main.js
+++ 3-js/assets/js/main.js
@@ -1,10 +1,24 @@
-// jQuery
-// $(document).ready(function() {
-  // code
-// });
+/* 
+/**
+ * Abrir e fechar o menu com o clique no botão Menu
+ */
+document.querySelector("#menu-button").addEventListener("click", () => {
+  document.querySelector("#menu").classList.toggle("-active");
+});
 
-// Vanilla JS
+/**
+ * Play do vídeo ao clicar na imagem de capa
+ */
+document.querySelector(".video").addEventListener("click", () => {
+  document.querySelector("#video-cover").classList.toggle("-inactive");
+});
 
-// window.onload = function() {
-  // code
-// };
+/**
+ *Sistema de sanfona para a lista
+ */
+
+document.querySelector(".item").addEventListener("click", () => {
+  document.querySelector(".item").classList.toggle("-active");
+});
+
+console.log(document.querySelectorAll(".item"));
